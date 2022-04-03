628bc04160838803c36239368851eadc2d63c1c2
Muhammad Osama <72892044+muhammadosama984@users.noreply.github.com>
2022-04-03 21:42:37 +0500

    Create Product.dart

---
+++ TechBy/lib/Models/product.dart
@@ -0,0 +1,11 @@
+class Product {
+  List<String> Image;
+  String title;
+  String description;
+  int Price;
+  String location;
+  DateTime uploadDate;
+
+  Product(this.Image, this.title, this.description, this.Price, this.location,
+      this.uploadDate);
+}
