89bc55a87558a9a8f08e3be100f5c50d56e5f065
HamzaFudda <humzafudda31@gmail.com>
2022-04-03 16:25:15 +0500

    1

---
+++ TechBy/lib/widgets/bottomBar.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class bottomNavBar extends StatefulWidget {
+  const bottomNavBar({Key? key}) : super(key: key);
+
+  @override
+  _bottomNavBarState createState() => _bottomNavBarState();
+}
+
+class _bottomNavBarState extends State<bottomNavBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
