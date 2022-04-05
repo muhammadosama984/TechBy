9db479b77cadb29b706ddc2f0f125914c1c0aca1
HamzaFudda <humzafudda31@gmail.com>
2022-04-06 01:34:46 +0500

    ads list

--- TechBy/lib/screens/ViewMyAds.dart
+++ TechBy/lib/screens/ViewMyAds.dart
@@ -11,45 +11,63 @@ class _ViewMyAdsState extends State<ViewMyAds> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("MyAds"),
-          automaticallyImplyLeading: false,
-        ),
-        body: Container(
-          child: ListView.builder(
-              itemCount: 10,
-              itemBuilder: ((context, index) {
-                return Card(
-                    child: GestureDetector(
-                  onTap: () {},
-                  child: Container(
-                    height: 130,
-                    child: ListTile(
-                      leading: Image(image: AssetImage('assets/TBicon.jpeg')),
-                      title: Text(
-                        "Title\n",
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.bold),
-                      ),
-                      subtitle: Text("Rs: Price \nYear: 2022 "),
-                      trailing: PopupMenuButton(
-                          icon: Icon(Icons.more_vert_rounded),
-                          itemBuilder: (context) => [
-                                PopupMenuItem(
-                                  onTap: () {},
-                                  child: Text("Change"),
-                                  value: 1,
-                                ),
-                                PopupMenuItem(
-                                  onTap: () {},
-                                  child: Text("Remove"),
-                                  value: 2,
-                                )
-                              ]),
-                    ),
+      appBar: AppBar(
+        title: Text("MyAds"),
+        automaticallyImplyLeading: false,
+      ),
+      body: _adsList(),
+    );
+  }
+}
+
+class _adsList extends StatefulWidget {
+  const _adsList({Key? key}) : super(key: key);
+
+  @override
+  _adsListState createState() => _adsListState();
+}
+
+class _adsListState extends State<_adsList> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: ListView.builder(
+        itemCount: 10,
+        itemBuilder: ((context, index) {
+          return Card(
+            child: GestureDetector(
+              onTap: () {},
+              child: Container(
+                height: 130,
+                child: ListTile(
+                  leading: Image(image: AssetImage('assets/TBicon.jpeg')),
+                  title: Text(
+                    "Title\n",
+                    style:
+                    TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                   ),
-                ));
-              })),
-        ));
+                  subtitle: Text("Rs: Price \nYear: 2022 "),
+                  trailing: PopupMenuButton(
+                    icon: Icon(Icons.more_vert_rounded),
+                    itemBuilder: (context) => [
+                      PopupMenuItem(
+                        onTap: () {},
+                        child: Text("Change"),
+                        value: 1,
+                      ),
+                      PopupMenuItem(
+                        onTap: () {},
+                        child: Text("Remove"),
+                        value: 2,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          );
+        }),
+      ),
+    );
   }
 }
