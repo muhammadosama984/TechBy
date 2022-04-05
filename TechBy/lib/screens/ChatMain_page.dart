db5e991842fd2e5f7187b01bfebb3415469102b7
HamzaFudda <humzafudda31@gmail.com>
2022-04-06 01:47:32 +0500

    chat

--- TechBy/lib/screens/ChatMain_page.dart
+++ TechBy/lib/screens/ChatMain_page.dart
@@ -12,7 +12,10 @@ class ChatMain_screen extends StatefulWidget {
 }
 
 List<ChatUser> data = [
-  ChatUser(name: "Ashar", product: "IPhone 13 Pro", image_name: "assets/smartphone.png"),
+  ChatUser(
+      name: "Ashar",
+      product: "IPhone 13 Pro",
+      image_name: "assets/smartphone.png"),
   ChatUser(name: "Osama", product: "Desktop", image_name: "assets/desktop.jpg")
 ];
 String tdata = DateFormat("HH:mm").format(DateTime.now());
@@ -36,82 +39,50 @@ class _ChatMain_screenState extends State<ChatMain_screen> {
       body: Center(
         child: Column(
           children: <Widget>[
-            Container(
-              padding: EdgeInsets.only(top: 20, left: 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: Text('All',),
-                    style: ElevatedButton.styleFrom(shape: StadiumBorder(),
-                        primary: Colors.grey.shade400,
-                        onPrimary: Colors.black),
-                  ),
-                  SizedBox(width: 20,),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: Text('Unread'),
-                    style: ElevatedButton.styleFrom(shape: StadiumBorder(),
-                        primary: Colors.grey.shade400,
-                        onPrimary: Colors.black),
-                  )
-                ],
-
-              ),
-
-            ),
-
+            _topPart(),
             Expanded(
                 child: ListView.builder(
-                  itemCount: data.length,
-                  itemBuilder: ((context, index){
-                    return Card(
-                        child: GestureDetector(
-                          onTap: (){
-                            Navigator.of(context).push(MaterialPageRoute(
-                                builder: (context) => Chat_screen()));
-                          },
-                          child: Container(
-                            height: 120,
-                            child: Column(
-                              children: <Widget>[
-                                ListTile(
-                                  leading: Image(image: AssetImage(data[index].image_name)),
-                                  title:Text(data[index].name) ,
-                                  subtitle: Text(data[index].product),
-                                  trailing: Column(
-                                    children: [
-                                      Text(tdata),
-                                      // SizedBox(height: 4,),
-
-                                    ],
-                                  ),
-                                ),
-                                Container(
-                                  alignment: Alignment(1.0,0.0),
-                                  child: PopupMenuButton(
-                                      icon: Icon(Icons.more_vert_rounded),
-                                      itemBuilder: (context) => [
-                                        PopupMenuItem(
-                                          child: Text("Delete"),
-                                          value: 1,
-                                        ),
-                                      ]
-                                  ),
-                                ),
-                              ],
-
-
-
-                            ),
+              itemCount: data.length,
+              itemBuilder: ((context, index) {
+                return Card(
+                    child: GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) => Chat_screen()));
+                  },
+                  child: Container(
+                    height: 120,
+                    child: Column(
+                      children: <Widget>[
+                        ListTile(
+                          leading:
+                              Image(image: AssetImage(data[index].image_name)),
+                          title: Text(data[index].name),
+                          subtitle: Text(data[index].product),
+                          trailing: Column(
+                            children: [
+                              Text(tdata),
+                            ],
                           ),
-                        )
-                    );
-                  }),
-                  padding: EdgeInsets.all(10),
-                )
-            ),
+                        ),
+                        Container(
+                          alignment: Alignment(1.0, 0.0),
+                          child: PopupMenuButton(
+                              icon: Icon(Icons.more_vert_rounded),
+                              itemBuilder: (context) => [
+                                    PopupMenuItem(
+                                      child: Text("Delete"),
+                                      value: 1,
+                                    ),
+                                  ]),
+                        ),
+                      ],
+                    ),
+                  ),
+                ));
+              }),
+              padding: EdgeInsets.all(10),
+            )),
           ],
         ),
       ),
@@ -119,10 +90,48 @@ class _ChatMain_screenState extends State<ChatMain_screen> {
   }
 }
 
-class ChatUser{
+class ChatUser {
   String name;
   String product;
   String image_name;
-  ChatUser({required this.name, required this.product, required this.image_name});
+
+  ChatUser(
+      {required this.name, required this.product, required this.image_name});
 }
 
+class _topPart extends StatelessWidget {
+  const _topPart({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(top: 20, left: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          ElevatedButton(
+            onPressed: () {},
+            child: Text(
+              'All',
+            ),
+            style: ElevatedButton.styleFrom(
+                shape: StadiumBorder(),
+                primary: Colors.grey.shade400,
+                onPrimary: Colors.black),
+          ),
+          SizedBox(
+            width: 20,
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text('Unread'),
+            style: ElevatedButton.styleFrom(
+                shape: StadiumBorder(),
+                primary: Colors.grey.shade400,
+                onPrimary: Colors.black),
+          )
+        ],
+      ),
+    );
+  }
+}
