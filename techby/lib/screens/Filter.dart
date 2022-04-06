639b17d190766000e06130089cc2dfec3145dadc
Osama <72892044+muhammadosama984@users.noreply.github.com>
2022-04-07 02:10:08 +0500

    Update filter.dart

--- techby/lib/screens/Filter.dart
+++ techby/lib/screens/Filter.dart
@@ -15,45 +15,103 @@ class _filterState extends State<filter> {
       appBar: AppBar(
         title: Text("Filter Items"),
       ),
-      body: Column(
-        children: [
-          Container(
-            margin: EdgeInsets.only(left: 70, right: 70),
-            child: TextFormField(
-              decoration: InputDecoration(hintText: "Location"),
+      body: Container(
+        margin: EdgeInsets.only(bottom: 50, top: 30),
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.only(left: 70, right: 70),
+              child: TextFormField(
+                decoration: InputDecoration(hintText: "Location"),
+              ),
             ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(
-                Icons.price_change,
-                size: 35,
-              ),
-              Text("Price Range"),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Container(
-                width: 100,
-                height: 30,
-                child: TextField(
-                  decoration: InputDecoration(hintText: "Min Price"),
+            SizedBox(
+              height: 100,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.price_change,
+                  size: 35,
+                ),
+                Text("Price Range"),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  width: 100,
+                  height: 30,
+                  child: TextField(
+                    decoration: InputDecoration(hintText: "Min Price"),
+                  ),
+                ),
+                Text("to"),
+                Container(
+                  width: 100,
+                  height: 30,
+                  child: TextField(
+                    decoration: InputDecoration(hintText: "Max Price"),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 100,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.calendar_today,
+                  size: 35,
+                ),
+                Text("Model Year"),
+              ],
+            ),
+            SizedBox(
+              height: 4,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  width: 100,
+                  height: 30,
+                  child: TextField(
+                    decoration: InputDecoration(hintText: "2000"),
+                  ),
+                ),
+                Text("to"),
+                Container(
+                  width: 100,
+                  height: 30,
+                  child: TextField(
+                    decoration: InputDecoration(hintText: "2022"),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 90,
+            ),
+            ButtonTheme(
+              height: 55,
+              child: RaisedButton(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
+                color: Colors.blue,
+                onPressed: (() {}),
+                child: Text(
+                  "Apply",
+                  style: TextStyle(fontSize: 18),
                 ),
               ),
-              Text("to"),
-              Container(
-                width: 100,
-                height: 30,
-                child: TextField(
-                  decoration: InputDecoration(hintText: "Max Price"),
-                ),
-              ),
-            ],
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
