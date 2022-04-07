c7b769c943a100595121956dfe9249d027a12722
HamzaFudda <humzafudda31@gmail.com>
2022-04-08 04:05:10 +0500

    horizontal list view

--- TechBy/lib/screens/mainPage.dart
+++ TechBy/lib/screens/mainPage.dart
@@ -91,70 +91,302 @@ class _categoriesState extends State<categories> {
   @override
   Widget build(BuildContext context) {
     return Container(
+      height: 180,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView(
+          scrollDirection: Axis.horizontal,
+          children: <Widget>[
+            _desktop(),
+            _mobile(),
+            _laptop(),
+            _camera(),
+            _monitor(),
+            _speaker(),
+
+          ],
+        ),
+      ),
+
+    );
+  }
+}
+class _desktop extends StatelessWidget {
+  const _desktop({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          Card(
+            child: GestureDetector(
+              onTap: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => ParticularProduct()));
+              },
+              child: Container(
+                height: 100.0,
+                width: 100.0,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/desktop.png'),
+                    fit: BoxFit.contain,
+                  ),
+                  shape: BoxShape.rectangle,
+                ),
+                //child: Text("Desktop"),
+              ),
+            ),
+          ),
+          Text(
+            "Desktop",
+            style: TextStyle(
+              //color: Colors.blue,
+              //fontWeight: FontWeight.bold,
+              fontFamily: 'Montserrat',
+              fontSize: 18,
+              //decoration: TextDecoration.underline,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+class _mobile extends StatelessWidget {
+  const _mobile({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          Card(
+            child: GestureDetector(
+              onTap: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => ParticularProduct()));
+              },
+              child: Container(
+                height: 100.0,
+                width: 100.0,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/smartphone.png'),
+                    fit: BoxFit.contain,
+                  ),
+                  shape: BoxShape.rectangle,
+                ),
+                //child: Text("Desktop"),
+              ),
+            ),
+          ),
+          Text(
+            "Mobiles",
+            style: TextStyle(
+              //color: Colors.blue,
+              //fontWeight: FontWeight.bold,
+              fontFamily: 'Montserrat',
+              fontSize: 18,
+              //decoration: TextDecoration.underline,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+class _laptop extends StatelessWidget {
+  const _laptop({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          Card(
+            child: GestureDetector(
+              onTap: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => ParticularProduct()));
+              },
+              child: Container(
+                height: 100.0,
+                width: 100.0,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/laptop.png'),
+                    fit: BoxFit.contain,
+                  ),
+                  shape: BoxShape.rectangle,
+                ),
+              ),
+            ),
+          ),
+          Text(
+            "Laptop",
+            style: TextStyle(
+              //color: Colors.blue,
+              //fontWeight: FontWeight.bold,
+              fontFamily: 'Montserrat',
+              fontSize: 18,
+              //decoration: TextDecoration.underline,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+class _camera extends StatelessWidget {
+  const _camera({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          Card(
+            child: GestureDetector(
+              onTap: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => ParticularProduct()));
+              },
+              child: Container(
+                height: 100.0,
+                width: 100.0,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/camera.png'),
+                    fit: BoxFit.contain,
+                  ),
+                  shape: BoxShape.rectangle,
+                ),
+              ),
+            ),
+          ),
+          Text(
+            "Camera",
+            style: TextStyle(
+              //color: Colors.blue,
+              //fontWeight: FontWeight.bold,
+              fontFamily: 'Montserrat',
+              fontSize: 18,
+              //decoration: TextDecoration.underline,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _monitor extends StatelessWidget {
+  const _monitor({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          Card(
+            child: GestureDetector(
+              onTap: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => ParticularProduct()));
+              },
+              child: Container(
+                height: 100.0,
+                width: 100.0,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/monitor.png'),
+                    fit: BoxFit.contain,
+                  ),
+                  shape: BoxShape.rectangle,
+                ),
+              ),
+            ),
+          ),
+          Text(
+            "Monitor",
+            style: TextStyle(
+              //color: Colors.blue,
+              //fontWeight: FontWeight.bold,
+              fontFamily: 'Montserrat',
+              fontSize: 18,
+              //decoration: TextDecoration.underline,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _speaker extends StatelessWidget {
+  const _speaker({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          Card(
+            child: GestureDetector(
+              onTap: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => ParticularProduct()));
+              },
+              child: Container(
+                height: 100.0,
+                width: 100.0,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/speaker.png'),
+                    fit: BoxFit.contain,
+                  ),
+                  shape: BoxShape.rectangle,
+                ),
+              ),
+            ),
+          ),
+          Text(
+            "Speaker",
+            style: TextStyle(
+              //color: Colors.blue,
+              //fontWeight: FontWeight.bold,
+              fontFamily: 'Montserrat',
+              fontSize: 18,
+              //decoration: TextDecoration.underline,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _container_1_categories extends StatelessWidget {
+  const _container_1_categories({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
       child: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Card(
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) => ParticularProduct()));
-                },
-                child: Container(
-                  height: 100.0,
-                  width: 100.0,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage('assets/pc.png'),
-                      fit: BoxFit.contain,
-                    ),
-                    shape: BoxShape.rectangle,
-                  ),
-                  //child: Text("Desktop"),
-                ),
-              ),
-            ),
-            Card(
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) => ParticularProduct()));
-                },
-                child: Container(
-                  height: 100.0,
-                  width: 100.0,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage('assets/smartphone.png'),
-                      fit: BoxFit.contain,
-                    ),
-                    shape: BoxShape.rectangle,
-                  ),
-                  //child: Text("Desktop"),
-                ),
-              ),
-            ),
-            Card(
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) => ParticularProduct()));
-                },
-                child: Container(
-                  height: 100.0,
-                  width: 100.0,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage('assets/laptop.png'),
-                      fit: BoxFit.contain,
-                    ),
-                    shape: BoxShape.rectangle,
-                  ),
-                ),
-              ),
-            ),
+            _desktop(),
+            _mobile(),
+            _laptop(),
           ],
         ),
       ),
