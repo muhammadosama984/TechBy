9b8d7f0d607a0b03d461104c85b34e94db038c77
Muhammad Osama <72892044+muhammadosama984@users.noreply.github.com>
2022-05-06 05:08:42 +0500

    User database

--- TechBy/lib/Models/ListofUsers.dart
+++ TechBy/lib/Models/ListofUsers.dart
@@ -1,8 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:techby/Models/Users.dart';
 import 'package:provider/provider.dart';
 
 class ListofUser with ChangeNotifier {
+  CollectionReference users = FirebaseFirestore.instance.collection("Users");
   List<User> userList = [];
   List<User> get returnUserList => userList;
 
@@ -11,12 +13,71 @@ class ListofUser with ChangeNotifier {
     notifyListeners();
   }
 
-  getUsers() {}
-  addUsers() {}
+  getUsers() async {
+    userList.clear();
+    await FirebaseFirestore.instance
+        .collection('Users')
+        .get()
+        .then((QuerySnapshot querySnapshot) {
+      querySnapshot.docs.forEach((doc) {
+        User temp = User.fromJson(doc.data() as Map<String, dynamic>);
+        temp.doc = doc.id;
+        print(doc.id);
+        // if(temp.done==true)
+        //   temp.completion_dateTime=DateTime.parse(doc.get("Completion Date"));
+        userList.add(temp);
+        //  print(tasks[0].title);
+      });
+    });
+  }
 
-  updateUser(User user) {}
+  addUsers(User user) {
+    //String a = user.Image;
 
-  deleteUser(User user) {}
+    //await _storage.ref('ProductImages/$a').putFile(product.Image);
+    users
+        .add({
+          'firstname': user.firstname,
+          'lastname': user.lastname,
+          'email': user.email,
+          'location': user.location,
+          'ImageUrl': user.ImageUrl,
+          'password': user.password,
+        })
+        .then((value) => print(""))
+        .catchError((error) => print("Failed to add user: $error"));
+  }
 
-  remove(User user) {}
+  updateUser(User user) {
+    DocumentReference documentReference =
+        FirebaseFirestore.instance.collection('Users').doc(user.doc);
+
+    final Map<String, dynamic> data = {};
+    data['firstname'] = user.firstname;
+    data['lastname'] = user.lastname;
+    data['email'] = user.email;
+    data['location'] = user.location;
+    data['ImageUrl'] = user.ImageUrl;
+    data['password'] = user.password;
+
+    // update data to Firebase
+    documentReference.update(data).whenComplete(() => print('updated'));
+  }
+
+  deleteUser(User user) async {
+    await FirebaseFirestore.instance
+        .collection('Users')
+        .doc(user.doc)
+        .delete()
+        .catchError((e) {
+      print(e);
+    }).whenComplete(() => print('deleted'));
+  }
+
+  remove(User user) async {
+    userList.remove(user);
+    await deleteUser(user);
+
+    notifyListeners();
+  }
 }
--- TechBy/lib/Models/Users.dart
+++ TechBy/lib/Models/Users.dart
@@ -1,7 +1,11 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 class User {
+  late List<File> Image;
   String firstname, lastname, email, location, ImageUrl, password, doc;
+  bool isApproved;
 
   User(
       {required this.firstname,
@@ -10,5 +14,18 @@ class User {
       required this.location,
       required this.ImageUrl,
       required this.password,
-      this.doc = ""});
+      this.doc = "",
+      this.isApproved = false});
+
+  static User fromJson(Map<String, dynamic> data) {
+    return User(
+        //needed to be set
+        firstname: data['firstname'],
+        lastname: data['lastname'],
+        email: data['email'],
+        location: data['location'],
+        ImageUrl: data['ImageUrl'],
+        password: data['password'],
+        isApproved: data['isApproved']);
+  }
 }
