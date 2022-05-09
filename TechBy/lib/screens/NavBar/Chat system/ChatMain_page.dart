15ebc39829744b76f09b65d578f01b6887b828ad
subhanbawany999 <subhanbawany999@gmail.com>
2022-05-09 19:24:57 +0500

    Chatrooms done

--- TechBy/lib/screens/NavBar/Chat system/ChatMain_page.dart
+++ TechBy/lib/screens/NavBar/Chat system/ChatMain_page.dart
@@ -1,7 +1,13 @@
+import 'dart:convert';
+import 'dart:developer';
+// import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
+// import 'package:techby/Models/Users.dart';
 import 'package:techby/widgets/Search.dart';
-
+import 'package:techby/Sign _In/google_sign_in.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'Chat_Page.dart';
 
 
@@ -13,15 +19,64 @@ class ChatMain_screen extends StatefulWidget {
 }
 
 List<ChatUser> data = [
-  ChatUser(
-      name: "Ashar",
-      product: "IPhone 13 Pro",
-      image_name: "assets/smartphone.png"),
-  ChatUser(name: "Osama", product: "Desktop", image_name: "assets/desktop.jpg")
+  // ChatUser(
+  //     name: "Ashar",
+  //     product: "IPhone 13 Pro",
+  //     image_name: "assets/smartphone.png"),
+  // ChatUser(name: "Osama", product: "Desktop", image_name: "assets/desktop.jpg")
 ];
 String tdata = DateFormat("HH:mm").format(DateTime.now());
 
 class _ChatMain_screenState extends State<ChatMain_screen> {
+  void initState() {
+    super.initState();
+    getChats();
+
+  }
+  //
+  // Static String fromJson(Map<String, dynamic> json) =>
+  //     title: json['title']);
+  // Future<List<ChatUser>>
+  void  getChats() async {
+    data.clear();
+    String email = await Provider.of<GoogleSingInProvider>(context, listen: false).emailAddress();
+    await FirebaseFirestore.instance.collection('Users').where('email', isEqualTo: email).get()
+        .then((QuerySnapshot querySnapshot) {
+      querySnapshot.docs.forEach((doc) {
+        //List<String> rooms = doc.data['rooms'] as;
+       var userMap = doc.data() as Map;
+       List<dynamic> rooms = userMap["rooms"];
+       rooms.forEach((room) async {
+         await FirebaseFirestore.instance.collection('Userrooms').doc(room).get()
+             .then((DocumentSnapshot documentSnapshot) {
+           var useroomMap = documentSnapshot.data() as Map;
+           //print(useroomMap);
+           if(useroomMap["buyerid"].toString() == email)
+              data.add(ChatUser(name: useroomMap["sellerid"], product: useroomMap["Product"], image_name: useroomMap["Image"], doc_id: documentSnapshot.id));
+            // data.add(abc);
+           else{
+             data.add(ChatUser(name: useroomMap["buyerid"], product: useroomMap["Product"], image_name: useroomMap["Image"], doc_id: documentSnapshot.id));
+           }
+           setState(() {
+
+           });
+           print(data);
+             });
+       });
+        // ads temp = ads.fromJson(doc.data() as Map<String, dynamic>);
+        // temp.docID = doc.id;
+
+
+        // data.add(temp);
+        //  print(tasks[0].title);
+      });
+    });
+
+
+
+    // List<DocumentReference> rooms = await current_user.rooms;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -91,13 +146,16 @@ class _ChatMain_screenState extends State<ChatMain_screen> {
   }
 }
 
+// class DocumentReferen
+
 class ChatUser {
   String name;
   String product;
   String image_name;
+  String doc_id;
 
   ChatUser(
-      {required this.name, required this.product, required this.image_name});
+      {required this.name, required this.product, required this.image_name, required this.doc_id});
 }
 
 class _topPart extends StatelessWidget {
