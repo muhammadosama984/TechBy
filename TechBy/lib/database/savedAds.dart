0bac246a2cc22ccbe1d195fa74a9ee7a2e803f78
HamzaFudda <humzafudda31@gmail.com>
2022-05-14 22:27:35 +0500

    saved ads

---
+++ TechBy/lib/database/savedAds.dart
@@ -0,0 +1,18 @@
+import 'package:techby/database/ads.dart';
+
+class savedAds {
+  String email;
+  ads Ad;
+
+  savedAds({required this.email, required this.Ad});
+
+  static savedAds fromJson(Map<String, dynamic> json) =>
+      savedAds(Ad: json['ads'], email: json['email']);
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = {};
+    data['ads'] = Ad;
+    data['email'] = email;
+    return data;
+  }
+}
