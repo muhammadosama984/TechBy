a231e2932fe3c0fb55c2ebc5f9cd39d7411cd424
Admanii <ahmedriaz-50@hotmail.com>
2022-05-15 18:43:02 +0500

    MainPage Functionality

--- TechBy/lib/main.dart
+++ TechBy/lib/main.dart
@@ -44,6 +44,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.white,
       ),
       home: Splash(),
     );
